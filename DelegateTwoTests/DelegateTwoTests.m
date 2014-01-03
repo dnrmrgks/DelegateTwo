4e311a3c28d677f8d8e970f4fe67f5cc1b1baf50
park sung chan <dnrmrgks@naver.com>
2014-01-03 19:30:29 +0900

    Initial Commit

---
+++ DelegateTwoTests/DelegateTwoTests.m
@@ -0,0 +1,34 @@
+//
+//  DelegateTwoTests.m
+//  DelegateTwoTests
+//
+//  Created by SDT-1 on 2014. 1. 3..
+//  Copyright (c) 2014년 T. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface DelegateTwoTests : XCTestCase
+
+@end
+
+@implementation DelegateTwoTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
